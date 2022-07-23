fb05eb9eca5c82caf4424c2def252c1130b7c700
Jezreel Sanchez <jezreelsanchez@apples-imac.attlocal.net>
2022-07-23 09:19:15 -0400

    feat: Add initial Swerve Drive logic

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,12 +4,107 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModule extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public SwerveModule() {
-    
+  private final TalonFX driveMotor;
+  private final TalonFX turningMotor;
+
+  private final CANCoder driveEncoder;
+  private final CANCoder turningEncoder;
+
+  private final PIDController turningPidController;
+
+  private AnalogInput absoluteEncoder;
+  private final boolean absoluteEncoderReversed;
+  private final double absoluteEncoderOffsetRad;
+  public SwerveModule(int driveMotorID, int turningMotorID, boolean driveMotorReversed, boolean turningMotorReversed, 
+          int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+            this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+            this.absoluteEncoderReversed = absoluteEncoderReversed;
+            absoluteEncoder = new AnalogInput(absoluteEncoderId);
+
+            driveMotor = new TalonFX(driveMotorID);
+            turningMotor = new TalonFX(turningMotorID);
+
+            driveMotor.setInverted(driveMotorReversed);
+            turningMotor.setInverted(turningMotorReversed);
+
+            driveEncoder = new CANCoder(driveMotorID);
+            turningEncoder = new CANCoder(turningMotorID);
+
+            driveEncoder.setPositionConversionFactor(ControlMode.Position, ModuleConstants.kDriveEncoderRot2Meter);
+            driveEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+            turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
+            turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+
+            turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
+            turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+            resetEncoders();
+  }
+  
+
+  public double getDrivePosition(){
+    return driveEncoder.getPosition();
+  }
+
+  public double getTurningPosition(){
+    return turningEncoder.getPosition();
+  }
+
+  public double getDriveVelocity(){
+    return driveEncoder.getVelocity();
+  }
+
+  public double getTurningVelocity(){
+    return turningEncoder.getVelocity();
+  }
+  public double getAbsoluteEncoderRad(){
+    double angle = absoluteEncoder.getVoltage() / RobotController.getVoltage5V();
+    angle *= 2.0 * Math.PI;
+    angle -= absoluteEncoderOffsetRad;
+    System.out.println(angle);
+    return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+  }
+
+  public void resetEncoders(){
+    driveEncoder.setPosition(0);
+    turningEncoder.setPosition(getAbsoluteEncoderRad());
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+  }
+  public void setDesiredState(SwerveModuleState state){
+    if(Math.abs(state.speedMetersPerSecond) < 0.001){
+      stop();
+      return;
+    }
+    state = SwerveModuleState.optimize(state, getState().angle);
+    driveMotor.set(ControlMode.PercentOutput, state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+    turningMotor.set(ControlMode.PercentOutput, turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+    SmartDashboard.putString("Swerve[" + absoluteEncoder.getChannel() + "] state", state.toString()); 
+  }
+  public void stop(){
+    driveMotor.set(ControlMode.PercentOutput, 0);
+    turningMotor.set(ControlMode.PercentOutput, 0);
   }
 
   @Override
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class SwerveSubsystem extends SubsystemBase {
+  /** Creates a new SwerveSubsystem. */
+  private final SwerveModule frontLeft = new SwerveModule(DriveConstants.kFrontLeftDriveMotorPort, DriveConstants.kFrontLeftTurningMotorPort, DriveConstants.kFrontLeftDriveEncoderReversed, 
+  DriveConstants.kFrontLeftTurningEncoderReversed, DriveConstants.kFrontLeftDriveAbsoluteEncoderPort, DriveConstants.kFrontLeftDriveAbsoluteEncoderOffset, 
+  DriveConstants.kFrontLeftDriveAbsoluteEncoderReversed);
+
+  private final SwerveModule frontRight = new SwerveModule(DriveConstants.kFrontRightDriveMotorPort, DriveConstants.kFrontRightTurningMotorPort, DriveConstants.kFrontRightDriveEncoderReversed, 
+  DriveConstants.kFrontRightTurningEncoderReversed, DriveConstants.kFrontRightDriveAbsoluteEncoderPort, DriveConstants.kFrontRightDriveAbsoluteEncoderOffset, 
+  DriveConstants.kFrontRightDriveAbsoluteEncoderReversed);
+
+  private final SwerveModule backLeft = new SwerveModule(DriveConstants.kBackLeftDriveMotorPort, DriveConstants.kBackLeftTurningMotorPort, DriveConstants.kBackLeftDriveEncoderReversed, 
+  DriveConstants.kBackLeftTurningEncoderReversed, DriveConstants.kBackLeftDriveAbsoluteEncoderPort, DriveConstants.kBackLeftDriveAbsoluteEncoderOffset, 
+  DriveConstants.kBackLeftDriveAbsoluteEncoderReversed);
+
+  private final SwerveModule backRight = new SwerveModule(DriveConstants.kBackRightDriveMotorPort, DriveConstants.kBackRightTurningMotorPort, DriveConstants.kBackRightDriveEncoderReversed, 
+  DriveConstants.kBackRightTurningEncoderReversed, DriveConstants.kBackRightDriveAbsoluteEncoderPort, DriveConstants.kBackRightDriveAbsoluteEncoderOffset, 
+  DriveConstants.kBackRightDriveAbsoluteEncoderReversed);
+
+  private WPI_Pigeon2 gyro = new WPI_Pigeon2(0);
+
+  public SwerveSubsystem() {
+    new Thread(() -> {
+      try {
+        Thread.sleep(1000);
+        zeroHeading();
+      } catch(Exception e){
+      }
+    }).start();
+  }
+  public void zeroHeading(){
+    gyro.reset();
+  }
+
+  public double getHeading(){
+    return Math.IEEEremainder(gyro.getAngle(), 360);
+  }
+  public Rotation2d getRotation2d(){
+    return Rotation2d.fromDegrees(getHeading());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Robot Heading", getHeading());
+  }
+  public void stopModules(){
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+  }
+  public void setModuleStates(SwerveModuleState[] desiredStates){
+    SwerveDriveKinematics.normalizeWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+    frontLeft.setDesiredState(desiredStates[0]);
+    frontRight.setDesiredState(desiredStates[1]);
+    backLeft.setDesiredState(desiredStates[2]);
+    backRight.setDesiredState(desiredStates[3]);
+  }
+}
